8aebe9cb5e7514bf5b089ecfe6ff18f63f8d30e9
Mitchell <mitchell65baker@gmail.com>
2021-02-04 14:44:06 -0600

    Reconfigured file structure

---
+++ board-portal-2/client/src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Router from "./components/Router";
+
+/**
+ * @name -
+ * @desc -
+ * @author -
+ * @date -
+ * @version -
+ * @param -
+ * @return -
+ */
+ReactDOM.render(
+  <React.StrictMode>
+    <Router />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
