7e8c9e135b2690530c3c035b1d08c21cbbdca849
Mitchell <mitchell65baker@gmail.com>
2021-03-08 14:05:25 -0600

    CSS for Header finished, working on Footer CSS

--- board-portal-2/src/components/Header/Header.js
+++ board-portal-2/src/components/Header/Header.js
@@ -2,41 +2,63 @@ import { Grid } from "@material-ui/core"
 import { BrowserRouter, Link } from "react-router-dom"
 import "./header.css"
 import scoreUpdates from "./img/ScoreUpdates.png"
+import steamIcon from "./img/steamicon.png"
+import singlplayerIcon from "./img/Singleplayer.png"
+import fullGameRunsIcon from "./img/running_large.png"
+import aggregatedIcon from "./img/Aggregated.jpg"
+import coopIcon from "./img/Co-op.png"
 
 const Header = () => {
   return (
-    <Grid
-      className='header'
-      container='true'
-      justify='space-between'
-      direction='column'>
-      <Grid container='true' direction='row'>
-        <Grid item='true' className='title'>
-          Portal 2 Leaderboards
-        </Grid>
-        <Grid item='true' className='nav'>
-          <div to='/home' className='nav-links'>
-            <img src={scoreUpdates} style={{ height: "25px" }} />
-            Score Updates
-          </div>
-          <div to='/home' className='nav-links'>
-            Single Player
-          </div>
-          <div to='/home' className='nav-links'>
-            Cooperative
-          </div>
-          <div to='/home' className='nav-links'>
-            Aggregated
-          </div>
-          <div to='/home' className='nav-links'>
-            Full Game Runs
-          </div>
+    <div className='header'>
+      <Grid container direction>
+        <Grid container direction='column'>
+          <Grid item className='title'>
+            Portal 2 Leaderboards
+          </Grid>
+          <Grid item className='nav'>
+            <div to='/home' className='nav-links'>
+              <img src={scoreUpdates} style={{ height: "25px" }} />
+              Score Updates
+            </div>
+            <div to='/home' className='nav-links'>
+              <img src={singlplayerIcon} style={{ height: "25px" }} />
+              Single Player
+            </div>
+            <div to='/home' className='nav-links'>
+              <img src={coopIcon} style={{ height: "25px" }} />
+              Cooperative
+            </div>
+            <div to='/home' className='nav-links'>
+              <img
+                src={aggregatedIcon}
+                style={{
+                  height: "25px",
+                  paddingLeft: ".5em",
+                  paddingRight: ".5em"
+                }}
+              />
+              Aggregated
+            </div>
+            <div to='/home' className='nav-links'>
+              <img
+                src={fullGameRunsIcon}
+                style={{
+                  height: "25px",
+                  paddingLeft: ".5em",
+                  paddingRight: ".5em"
+                }}
+              />
+              Full Game Runs
+            </div>
+          </Grid>
         </Grid>
       </Grid>
-      <Grid item='true' className='steam'>
-        Steam
+      <Grid item className='steam'>
+        <div className='steam-text'>Steam</div>
+        <img src={steamIcon} style={{ height: "45px" }} />
       </Grid>
-    </Grid>
+    </div>
   )
 }
 
