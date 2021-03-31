8419393fc73eff9f42bcee3e487a89d3adaaa6e7
Pixel-Knight <61557331+Pixel-Knight@users.noreply.github.com>
2021-03-31 16:59:49 -0500

    Logging Added
    Basic event logging now works - simply displays it in the terminal, using actix-web::middleware::Logging and env_logger.
    Next, I will try adding the ability to use command-line arguments to set logging to go to a file instead of directly to the terminal.

--- server/src/main.rs
+++ server/src/main.rs
@@ -6,7 +6,8 @@ mod handlers;
 #[macro_use]
 extern crate serde_json;
 
-use actix_web::{get, body::Body, http::header, web, App, HttpRequest, HttpServer,HttpResponse, Responder, Error};
+use actix_web::{get, body::Body, http::header, web, App, HttpRequest, HttpServer,HttpResponse, Responder, Error, middleware::Logger};
+use env_logger::Env;
 use dotenv::dotenv;
 
 #[actix_web::main]
@@ -14,8 +15,15 @@ async fn main() -> std::io::Result<()> {
     dotenv().ok();
     let config = crate::config::Config::from_env().unwrap();
 
+    // Initializes Logger with "default" format:  %a %t "%r" %s %b "%{Referer}i" "%{User-Agent}i" %T
+    // Remote-IP, Time, First line of request, Response status, Size of response in bytes, Referer, User-Agent, Time to serve
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
+    println!("Server starting at http://{}:{}/", config.server.host, config.server.port);
+
     HttpServer::new(|| {
         App::new()
+        	.wrap(Logger::default())
             .configure(handlers::init)
     })
     .bind(format!("{}:{}", config.server.host, config.server.port))?
