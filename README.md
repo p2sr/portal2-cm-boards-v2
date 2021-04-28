6bb7a1f654eb805d8352cd027bb3a523425613b6
DanielBatesJ <DanielBatesJ@gmail.com>
2021-04-28 00:56:40 -0500

    More cleaning of excess and fixing of readme

--- README.md
+++ README.md
@@ -1,35 +1,48 @@
-# portal-2-boards-capstone-CSCE4901
+# Portal 2 Leaderboards Revision (board.portal2.sr)
 
-## Group Memebers
+## Building
+
+### Backend
+The backend binary can be build by using `cargo build` in the `backend` directory. With Rust installed, it should download all dependancies and compile the binary for you.
+#### Features
+* Pulling Official Single Player Map data from Steam, caching that data to avoid needing to re-parse/compare.
+* Supports multithreading with Rayon.
+* Queries to the API for comparison data.
+#### Future Plans
+Plans for the future include allowing args for the binary to allow it to distinguish between different tasks, but the current goal is to use it for fetching / process / cache times from the 
+Steam Leaderboards, as well as to calculate and cache profile data for all players.
+
+The purpose of keeping this backend seperate from the web-server is to off-load some more computationally heavy tasks to an entirely different process for modularity. None of this design is final
+### Server
+The server can be started with `cargo run` in the `server` directory. You will need diesel installed, and a connection to your MySQL server. You should run migration setup for diesel
+so the Portal 2 Leaderboards are populated in MySQL for you. 
+
+Be sure to copy the `.env.example` file, remove `.example` from the file name, and change the contents of the file to suite your usecase.
+#### Features:
+* Endpoints for sp/coop/changelog pages.
+* Supports db pool and async for non-blocking, fast response to simultanious queries.
+#### Future Plans
+* Result Caching.
+* Database / Filtering reworks pending changes to [diesel.rs](https://github.com/diesel-rs/diesel).
+* Authentication handling through Steam.
+* Permissions handling for Admin users.
+* Category integration.
+* Player Profiles.
+
+### Front-end
+The front-end build can be done with `npm install` in the `/board-portal-2` folder and once dependancies are installed, the client server can be started with `npm start`
+#### Features
+* Supports querying a running webserver on it's given endpoints for changelog, preview pages, sp maps and coop maps.
+* Light and dark theme support.
+* Prototyped page designs for many auxiliary pages.
+
+## Original Project Team
+This project started as a Senior Capstone Project for the following members.
 * Daniel Bates   -> @DanielBatesJ
 * Mitchell Baker -> @MitchellBaker65
 * Josh Bednarz   -> @Pixel-Knight
 * Michael Murphy -> @MurphyMichael
 * John Fiedler   -> @JFiedler23
-
-## Building
-
-### Backend
-The backend binary can be build by using `cargo build` in the `/backend` directory. With Rust installed, it should download all dependancies and compile the binary for you.
-Current features from running this binary:
-* Will create all SP map JSON information in `/server/api/maps/sp`
-* Planned for the future is support for specifying the functions with environment variables (such as adding new times, updating specific maps etc)
-
-### Server
-The server can be started with `cargo run` in the `server` directory in much the same way as the backend. This should keep a server running on the terminal instance you ran it on.
-
-Be sure to copy the `.env.example` file, remove `.example` from the file name, and change the contents of the file to suite your usecase.
-
-Current Features:
-* Listens on the specified port for your host for GET requests for sp maps. The endpoint is `/api/maps/sp/{mapid}` where the mapid is the Steamleaderboard ID for the singleplayer map.
-
-### Front-end
-
-The front-end build can be done with `npm install` in the `/board-portal-2` folder and once dependancies are installed, the client server can be started with `npm start`
-
-## Project Specs
-[Software Requirements Specifications](https://docs.google.com/document/d/1HnGGvk6OQsIHrAGKlBZczCmHcyulmhzpf-vH-jAk1AU/edit?usp=sharing)
-
 
 ## Project References 
 [Original Portal 2 Boards](https://github.com/iVerb1/Portal2Boards)
---
+++ db-extras/dbscript/sqltestqueries.sql
@@ -0,0 +1,85 @@
+SELECT coopbundled.time_gained, coopbundled.profile_number1, coopbundled.profile_number2, coopbundled.score FROM coopbundled INNER JOIN usersnew ON coopbundled.profile_number1=usersnew.profile_number WHERE coopbundled.banned=0 AND usersnew.banned=0 AND map_id=47856 ORDER BY coopbundled.score ASC;
+
+SELECT coopbundled.time_gained, coopbundled.score, usersnew.boardname, u2.boardname (CASE WHEN coopbundled.profile_number2 = "" THEN "NONE" ELSE u2.boardname END) FROM coopbundled JOIN usersnew ON coopbundled.profile_number1=usersnew.profile_number JOIN usersnew AS u2 ON coopbundled.profile_numer2=u2.profile_number WHERE coopbundled.banned=0 AND usersnew.banned=0 AND u2.banned=0 AND map_id=47856 ORDER BY coopbundled.score ASC;
+/*Only prints when both the names exists, adapt to handle having no boardname default to steamname*/
+SELECT  coopbundled.time_gained
+       ,maps.name
+       ,coopbundled.score
+       ,usersnew.steamname
+       ,u2.steamname
+FROM coopbundled
+JOIN maps
+ON coopbundled.map_id=maps.steam_id
+JOIN usersnew
+ON coopbundled.profile_number1=usersnew.profile_number
+JOIN usersnew u2
+ON coopbundled.profile_number2=u2.profile_number 
+WHERE coopbundled.banned=0 
+AND usersnew.banned=0 
+AND u2.banned=0
+AND map_id=52642
+ORDER BY coopbundled.score ASC; 
+
+
+INSERT INTO usersnew
+
+
+/*Changelog*/
+FOREIGN KEY (`profile_number`) REFERENCES usersnew(`profile_number`),
+FOREIGN KEY (`map_id`) REFERENCES maps(`steam_id`),
+FOREIGN KEY (`coopid`) REFERENCES coopbundled(`id`);
+
+/*Coopbundled*/
+FOREIGN KEY (`changelogid1`) REFERENCES changelog(`id`),
+FOREIGN KEY (`changelogid2`) REFERENCES changelog(`id`),
+FOREIGN KEY (`profile_number1`) REFERENCES usersnew(`profile_number`),
+FOREIGN KEY (`profile_number2`) REFERENCES usersnew(`profile_number`),
+FOREIGN KEY (`map_id`) REFERENCES maps(`steam_id`),
+FOREIGN KEY (`previous_id1`) REFERENCES changelog(`previous_id`),
+FOREIGN KEY (`previous_id2`) REFERENCES changelog(`previous_id`);
+
+/*Maps*/
+FOREIGN KEY (`chapter_id`) REFERENCES chapters(`id`);   
+
+/*scores*/
+FOREIGN KEY (`map_id`) REFERENCES maps(`steam_id`),
+FOREIGN KEY (`profile_number`) REFERENCES usersnew(`profile_number`),
+FOREIGN KEY (`changelog_id`) REFERENCES changelog(`id`);
+
+/*scores acts as a container for joins for the changelog entities.
+
+
+ALTER TABLE scores ADD CONSTRAINT `scores_ibfk_3` FOREIGN KEY (`changelog_id`) REFERENCES `changelog` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+*/
+
+/*
+mysql> use p2boards;
+Database changed
+mysql> ALTER TABLE changelog
+    -> ADD CONSTRAINT `changelog_ibfk_1` FOREIGN KEY (`profile_number`) REFERENCES `usersnew` (`profile_number`) ON DELETE CASCADE ON UPDATE CASCADE;
+ERROR 1452 (23000): Cannot add or update a child row: a foreign key constraint fails (`p2boards`.`#sql-107_19`, CONSTRAINT `changelog_ibfk_1` FOREIGN KEY (`profile_number`) REFERENCES `usersnew` (`profile_number`) ON DELETE CASCADE ON UPDATE CASCADE)
+mysql> INSERT INTO `usersnew` VALUES ('', NULL, NULL, 0, 0, NULL, NULL, NULL, NULL, 0, NULL);
+Query OK, 1 row affected (0.22 sec)
+
+mysql> ALTER TABLE changelog ADD CONSTRAINT `changelog_ibfk_1` FOREIGN KEY (`profile_number`) REFERENCES `usersnew` (`profile_number`) ON DELETE CASCADE ON UPDATE CASCADE;
+ERROR 1452 (23000): Cannot add or update a child row: a foreign key constraint fails (`p2boards`.`#sql-107_19`, CONSTRAINT `changelog_ibfk_1` FOREIGN KEY (`profile_number`) REFERENCES `usersnew` (`profile_number`) ON DELETE CASCADE ON UPDATE CASCADE)
+mysql> SELECT profile_number FROM changelog WHERE profile_number NOT IN (SELECT profile_number FROM usersnew);
++-------------------+
+| profile_number    |
++-------------------+
+| 76561197972048348 |
++-------------------+
+1 row in set (0.53 sec)
+
+mysql> INSERT INTO `usersnew` VALUES ('76561197972048348', NULL, NULL, 0, 0, NULL, NULL, NULL, NULL, 0, NULL);
+Query OK, 1 row affected (0.23 sec)
+
+mysql> ALTER TABLE changelog ADD CONSTRAINT `changelog_ibfk_1` FOREIGN KEY (`profile_number`) REFERENCES `usersnew` (`profile_number`) ON DELETE CASCADE ON UPDATE CASCADE;
+Query OK, 96793 rows affected (46.48 sec)
+Records: 96793  Duplicates: 0  Warnings: 0
+
+mysql> ALTER TABLE changelog ADD CONSTRAINT `changelog_ibfk_2` FOREIGN KEY (`map_id`) REFERENCES `maps` (`steam_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+mysql> ALTER TABLE changelog ADD CONSTRAINT `changelog_ibfk_3` FOREIGN KEY (`coopid`) REFERENCES `coopbundled` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+mysql>*/
