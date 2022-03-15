aa901caaccfbdeb81ca9536d3c1eefb9524d287a
Daniel Bates <36864021+DanielBatesJ@users.noreply.github.com>
2022-03-15 16:18:22 -0500

    init Vue

---
+++ frontend/p2-boards/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="app">p2boards</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "App",
+  compontents: {},
+});
+</script>
---
+++ frontend/p2-boards/src/views/HomeView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="home">p2 boards</div>
+</template>
+
+<script lang="ts">
+</script>
