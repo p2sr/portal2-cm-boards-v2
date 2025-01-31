7440854f87bb3f396c16427cd4713c522d6300ac
William Koivula <wkoivula@kth.se>
2025-01-31 18:33:07 +0100

    Added simple page handling of Overall page

--- frontend/src/components/LeaderboardEntries.jsx
+++ frontend/src/components/LeaderboardEntries.jsx
@@ -3,12 +3,19 @@ import { Box, useTheme, Typography } from "@mui/material";
 import { tokens } from "../theme";
 import LeaderboardIcon from '@mui/icons-material/Leaderboard';
 import LeaderboardEntry from './LeaderboardEntry';
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
+import { useState } from 'react';
+import { IconButton } from "@mui/material";
 
 const LeaderboardEntries = props => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
 
-    var index = 0;
+    const [page, setPage] = useState(0);
+
+    var pageEntries = 40;
+    var index = page * pageEntries;
 
     var list = props.data;
     var type = props.type;
@@ -19,7 +26,7 @@ const LeaderboardEntries = props => {
         list.sort((a,b) => a[1].score - b[1].score)
     }
 
-    const limitList = list.slice(0, 40);
+    const limitList = list.slice(page * pageEntries, (page + 1) * pageEntries);
 
     console.log(limitList);
 
@@ -85,6 +92,32 @@ const LeaderboardEntries = props => {
                 alignItems="center"
                 justifyContent="center"
                 >
+                    <Box
+                    display="flex"
+                    padding="10px"
+                    flexGrow="1"
+                    backgroundColor={colors.primary[700]}
+                    style={{borderTopLeftRadius:"10px", borderTopRightRadius:"10px"}}
+                    justifyContent="center"
+                    alignItems="center">
+                        <IconButton disableRipple>
+                            <ArrowLeftIcon
+                                onClick={() =>setPage(page - 1 < 0 ? page : page - 1)}
+                            />
+                        </IconButton>
+                        <Typography
+                            variant="h5"
+                            color={colors.gray[100]}
+                            fontWeight="regular"
+                            >
+                            {page} / {Math.floor(list.length / pageEntries)}
+                        </Typography>
+                        <IconButton disableRipple>
+                            <ArrowRightIcon
+                                onClick={() => setPage(page + 1 > Math.floor(list.length / pageEntries) ? page : page + 1)}
+                            />
+                        </IconButton>
+                    </Box>
                     {leaderboard}
                 </div>
             </Box>
